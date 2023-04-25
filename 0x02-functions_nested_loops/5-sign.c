e9046f31b5838802da6c466bec420797f0d96a0c
ceeygithub <cynthiaekhomwandolor@gmail.com>
2023-04-25 08:35:27 -0700

    task 5 correction

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,25 +1,24 @@
 #include "main.h"
+
 /**
-*print_sign -  prints the sign of a number.
-*@n: integer varaible
-*Return: 1 if n>0, 0 if n == 0, -1 if n<0
-*/
+ * print_sign - Entr point
+ * @n: carrier variable
+ * Description: prints the sign of a number
+ * Return: 1 if n>0, 0 if n == 0, -1 if n<0
+ */
+
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar (43);
+		_putchar('+');
 		return (1);
 	}
 	else if (n < 0)
 	{
-		_putchar (48);
+		_putchar('-');
 		return (-1);
 	}
-	else
-	{
-		_putchar (48);
-		return (0);
-	}
-	_putchar('\n');
+	_putchar('0');
+	return (0);
 }
